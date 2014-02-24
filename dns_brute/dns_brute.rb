cfa60a9815ffe8083fb11f007ce8d32dcec90074
hal3002 <hal@nutsec.org>
2014-02-24 13:03:37 -0800

    Added nameserver options and queries different types of records

--- dns_brute/dns_brute.rb
+++ dns_brute/dns_brute.rb
@@ -11,7 +11,8 @@ threads = Array.new
 # Defaults
 options = {
 	:threads => 30,
-	:depth => 3
+	:depth => 3,
+	:nameservers => [],
 }
 
 OptionParser.new do |opts|
@@ -21,15 +22,22 @@ OptionParser.new do |opts|
 	opts.on("-d", "--domain DOMAINS", "The target domain to scan.") do |v|
 		options[:domain] = v
 	end
-	opts.on("-D", "--depth DEPTH", "The number of characters deep to go. (Default: #{options[:depth]}") do |v|
+	opts.on("-D", "--depth DEPTH", "The number of characters deep to go. (Default: #{options[:depth]})") do |v|
 		options[:depth] = v.to_i
 	end
 	opts.on("-w", "--wordlist WORDLIST", "Optional dictionary list to test.") do |v|
 		options[:wordlist] = v
 	end
+	opts.on("-n", "--nameserver NAMESERVER", "DNS server to use for lookups. You can specify this multiple times. (Default: 8.8.8.8)") do |v|
+		options[:nameservers] << v
+	end
 end.parse!
 
+# Make sure the domain is specified
 abort "Domain is required. Try --help for more information" if options[:domain].nil?
+
+# Add a default nameserver if there isn't one
+options[:nameservers] << "8.8.8.8" if options[:nameservers].empty?
 
 # Generate all hostnames and add them to the work queue
 (options[:depth] + 1).times do |i|
@@ -41,17 +49,31 @@ end
 # Add a dictionary if we are also doing that
 unless options[:wordlist].nil?
 	File.read(options[:wordlist]).each_line do |line|
-		req_queue << "#{line.chomp}.#{options[:domain]}"
+		req_queue << "#{line.chomp}.#{options[:domain]}."
 	end
 end
 
 # Create worker threads to do our querying
 options[:threads].times do
 	threads << Thread.new do
+
+		# Create our resolver for each thread
+		dns = Resolv::DNS.new(:nameserver => options[:nameservers])
+		
 		until req_queue.empty?
 			begin
 				req = req_queue.pop
-				res_queue << "#{req},#{Resolv.getaddress(req)}"
+				dns.getresources(req, Resolv::DNS::Resource::IN::ANY).each do |r|
+					c = r.class.to_s.split(/::/).last
+					case c
+					when 'CNAME'
+						res_queue << "#{req} #{r.ttl} IN #{c} #{r.name}"
+					when 'A'
+						res_queue << "#{req} #{r.ttl} IN #{c} #{r.address}"
+					when 'AAAA'
+						res_queue << "#{req} #{r.ttl} IN #{c} #{r.address}"
+					end
+				end
 			rescue Resolv::ResolvError => e
 				# Ignore as it's just a failed query
 			end
